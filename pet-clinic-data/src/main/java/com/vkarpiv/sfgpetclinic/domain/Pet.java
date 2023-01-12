89637b06ab29da65f464c32a1343a3daecc7014f
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2023-01-12 10:30:47 +0200

    Implement base entity

--- pet-clinic-data/src/main/java/com/vkarpiv/sfgpetclinic/domain/Pet.java
+++ pet-clinic-data/src/main/java/com/vkarpiv/sfgpetclinic/domain/Pet.java
@@ -2,7 +2,7 @@ package com.vkarpiv.sfgpetclinic.domain;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
   private PetType petType;
   private Owner owner;
--- pet-clinic-data/src/main/java/com/vkarpiv/sfgpetclinic/domain/PetType.java
+++ pet-clinic-data/src/main/java/com/vkarpiv/sfgpetclinic/domain/PetType.java
@@ -1,6 +1,6 @@
 package com.vkarpiv.sfgpetclinic.domain;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
   private String name;
 
